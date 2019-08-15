a8ee0db803149f081771116ad72c8a64bae20f8d
weolix <45514648+weolix@users.noreply.github.com>
2019-08-15 11:51:14 +0800

    Update 24to12.c

--- 24to12.c
+++ 24to12.c
@@ -1,9 +1,2 @@
-#include<stdio.h>
-main() {
-	int hour,mnt;
-	printf("enter your time(24): ");
-	scanf_s("%d:%d", &hour, &mnt);
-
-	printf("the time(12) is %d:%d", hour > 12 ? hour - 12 : hour, mnt);
-
-}
+#######################################################
+Who Can Help Me？
